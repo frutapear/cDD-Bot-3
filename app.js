8f0ff3bcea8945b6fe6e5931ec5baa22372f4b50
frutapear <hinojosaggaby@gmail.com>
2024-07-23 11:43:18 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
-const { createBot, createProvider, createFlow, addKeyword, EVENTS } = require('@bot-whatsapp/bot');
-require('dotenv').config(); // Correctly invoking dotenv configuration
+const { createBot, createProvider, createFlow, addKeyword, EVENTS } = require('@bot-whatsapp/bot')
+require('dotenv').config
 
-const QRPortalWeb = require('@bot-whatsapp/portal');
-const BaileysProvider = require('@bot-whatsapp/provider/baileys');
-const MockAdapter = require('@bot-whatsapp/database/mock');
+const QRPortalWeb = require('@bot-whatsapp/portal')
+const BaileysProvider = require('@bot-whatsapp/provider/baileys')
+const MockAdapter = require('@bot-whatsapp/database/mock')
 
-const path = require("path");
-const fs = require("fs");
 
-// DECLARACIONES PATH/OBJECTS //
-const menuPath = path.join(__dirname, "mensajes", "menu.txt");
-const menu = fs.readFileSync(menuPath, "utf-8");
+const path = require("path")
+const fs = require("fs")
 
-const cuentasPath = path.join(__dirname, "mensajes", "cuentas.txt");
-const cuentas = fs.readFileSync(cuentasPath, "utf-8");
 
-const carteraPath = path.join(__dirname, "mensajes", "cartera.txt");
-const cartera = fs.readFileSync(carteraPath, "utf-8");
+//DECLARACIONES PATH/OBJECTS//
+const menuPath = path.join(__dirname, "mensajes", "menu.txt")
+const menu = fs.readFileSync(menuPath, "utf-8")
 
-const resultadosPath = path.join(__dirname, 'mensajes', 'resultados.txt');
-const resultados = fs.readFileSync(resultadosPath, 'utf-8');
+const cuentasPath = path.join(__dirname, "mensajes", "cuentas.txt")
+const cuentas = fs.readFileSync(cuentasPath, "utf-8")
 
-// DECLARACIONES FLOW E INFO //
-// FLOW PRINCIPAL
+const carteraPath = path.join(__dirname, "mensajes", "cartera.txt")
+const cartera = fs.readFileSync(carteraPath, "utf-8")
+
+const resultadosPath = path.join(__dirname, 'mensajes', 'resultados.txt')
+const resultados = fs.readFileSync(resultadosPath, 'utf-8')
+
+//DECLARACIONES FLOW E INFO//
+//FLOW PRINCIPAL
 const flowPrincipal = addKeyword(['hola', 'ole', 'alo', 'Hola', 'Buenas'])
     .addAnswer('Gracias por comunicarse con el Dr. Baralt')
     .addAnswer('En estos momentos no podemos atender su mensaje, a la mayor brevedad posible lo haremos, Dios mediante.')
@@ -33,118 +35,177 @@ const flowPrincipal = addKeyword(['hola', 'ole', 'alo', 'Hola', 'Buenas'])
         '🔔 El doctor llega a la consulta a partir de las 10:00 AM',
         '*Dios le bendiga*'
     ])
-    .addAnswer('Para más información, por favor, escriba: *Menú*.');
+    .addAnswer('Para mas informacion, por favor, escriba: *Menú*.')
 
-// OPCION 1 DEL MENU - AGENDAR CITAS //
-const flowCuenta = addKeyword(['1'])
+
+//OPCION 1 DEL MENU - AGENDAR CITAS//
+const flowCuenta = addKeyword(EVENTS.ACTION)
     .addAnswer('Si desea agendar una cita debe realizar el pago de la consulta a una de nuestras cuentas.')
-    .addAnswer('Para continuar, escriba *Ok*', { capture: true }, (ctx, { fallBack }) => {
-        if (!ctx.body.toLowerCase().includes('ok')) {
-            return fallBack();
-        }
-        console.log('Mensaje entrante:', ctx.body);
+    .addAnswer ('Para continuar, escriba *Ok*', {capture:true}, (ctx, {fallBack}) => {
+      if (!ctx.body.toLowerCase().includes('ok')) {
+          return fallBack();
+      }
+      console.log('Mensaje entrante:', ctx.body);
     })
     .addAnswer('Enviando Cuentas Bancarias (BHD, Popular, Banreservas).')
-    .addAnswer('Responda *"cuentas"* para continuar', { capture: true }, (ctx, { fallBack }) => {
-        if (!ctx.body.toLowerCase().includes('cuentas')) {
-            return fallBack();
-        }
-        console.log('Mensaje entrante:', ctx.body);
-    });
-
-// FLOW DE CUENTAS //
-const flowCuentasB = addKeyword(['cuentas', '1'])
-    .addAnswer([
-        "BANRESERVAS",
-        '*Cuenta de Ahorros*',
-        'NO. 9605876089',
-        'Natalia Guerrero',
-        '001-1164182-5'
-    ])
-    .addAnswer("Por favor, debe responder este mensaje con una captura de pantalla de la transacción completada con el *MONTO TOTAL DE LA CONSULTA*", { capture: true }, (ctx, { fallBack }) => {
-        if (!ctx.message?.imageMessage) {
-            return fallBack();
-        }
-        console.log('Mensaje entrante:', ctx.body);
+    .addAnswer('Responda *"cuentas"* para continuar', {capture:true}, (ctx, {fallBack}) => {
+      if (!ctx.body.toLowerCase().includes('cuentas')) {
+          return fallBack();
+      }
+      console.log('Mensaje entrante:', ctx.body);
     })
-    .addAnswer(['Captura recibida ✅ ',
-        'Por favor, envíenos su nombre completo y qué seguro médico utiliza.',
-        'Por ejemplo: *Maria Gonzalez Perez, Seguro Senasa*'
-    ]);
 
-// Add similar setup for flowCuentasBHD and flowCuentasP here
-//...
+//FLOW DE CUENTAS//
+const flowCuentasB = addKeyword (EVENTS.ACTION)
+.addAnswer(["BANRESERVAS",
+  '*Cuenta de Ahorros*',
+  'NO. 9605876089',
+  'Natalia Guerrero',
+  '001-1164182-5'
+])
+.addAnswer("Por favor, debe responder este mensaje con una captura de pantalla de la transaccion completada con el *MONTO TOTAL DE LA CONSULTA*", {capture:true}, (ctx, {fallBack}) => {
+  if (!ctx.message?.imageMessage) {
+      return fallBack();
+  }
+  console.log('Mensaje entrante:', ctx.body);
+})
+.addAnswer(['Captura recibida ✅ ',
+  'Por favor, envienos su nombre completo y que seguro medico utiliza.',
+  'Por ejemplo: *Maria Gonzalez Perez, Seguro Senasa*'
+])
 
-const cuentasFlow = addKeyword(['cuentas']).addAnswer(
-    cuentas,
-    { capture: true },
-    async (ctx, { gotoFlow, fallBack, flowDynamic }) => {
+const flowCuentasBHD = addKeyword (EVENTS.ACTION)
+.addAnswer(["Banco BHD",
+  '*Cuenta de Ahorros*',
+  'NO. 32385280010',
+  'Gabriela Hinojosa',
+  '402-3246-332-9'
+])
+.addAnswer("Por favor, debe responder este mensaje con una captura de pantalla de la transaccion completada con el *MONTO TOTAL DE LA CONSULTA*", {capture:true}, (ctx, {fallBack}) => {
+  if (!ctx.message?.imageMessage) {
+      return fallBack();
+  }
+  console.log('Mensaje entrante:', ctx.body);
+})
+.addAnswer(['Captura recibida ✅ ',
+  'Por favor, envienos su nombre completo y que seguro medico utiliza.',
+  'Por ejemplo: *Maria Gonzalez Perez, Seguro Senasa*'
+])
+
+const flowCuentasP = addKeyword (EVENTS.ACTION)
+.addAnswer(["Banco Popular",
+  '*Cuenta de Ahorros*',
+  'NO. 809370745',
+  'Julio F. Baralt',
+  '001-0720802-7'
+])
+.addAnswer("Por favor, debe responder este mensaje con una captura de pantalla de la transaccion completada con el *MONTO TOTAL DE LA CONSULTA*", {capture:true}, (ctx, {fallBack}) => {
+  if (!ctx.message?.imageMessage) {
+      return fallBack();
+  }
+  console.log('Mensaje entrante:', ctx.body);
+})
+.addAnswer(['Captura recibida ✅ ',
+  'Por favor, envienos su nombre completo y que seguro medico utiliza.',
+  'Por ejemplo: *Maria Gonzalez Perez, Seguro Senasa*'
+])
+
+ const cuentasFlow = addKeyword(['cuentas']).addAnswer(
+     cuentas,
+      { capture: true },
+      async (ctx, { gotoFlow, fallBack, flowDynamic }) => {
         if (!["1", "2", "3"].includes(ctx.body)) {
-            return fallBack(
-                "Respuesta no válida, por favor selecciona una de las opciones."
-            );
+          return fallBack(
+            "Respuesta no válida, por favor selecciona una de las opciones."
+          );
         }
         switch (ctx.body) {
-            case "1":
-                return gotoFlow(flowCuentasB);
-            case "2":
-                return gotoFlow(flowCuentasBHD);
-            case "3":
-                return gotoFlow(flowCuentasP);
-            case "0":
-                return await flowDynamic(
-                    "Saliendo... Puedes volver a acceder al menú escribiendo *menu*"
-                );
+          case "1":
+            return gotoFlow(flowCuentasB);
+          case "2":
+            return gotoFlow(flowCuentasBHD);
+          case "3":
+            return gotoFlow(flowCuentasP);
+          case "0":
+            return await flowDynamic(
+              "Saliendo... Puedes volver a acceder al menú escribiendo *menu*"
+            );
         }
-    }
-);
+      }
+  );
+  
 
-// Define other flow variables here (flowServ, flowPap, etc.)
 
-const menuFlow = addKeyword(['menu', 'Menu', 'Menú']).addAnswer(
+//OPCION 2 DEL MENU / SERVICIOS //
+const flowServ = addKeyword(EVENTS.ACTION).addAnswer(
+    cartera,
+     { capture: true })
+
+//OPCION 3 DEL MENU / RESULTADOS PAP/BIOPSIA//
+const flowPap = addKeyword(EVENTS.ACTION).addAnswer(
+  resultados,
+   { capture: true })
+
+//OPCION 4 DEL MENU / DUDAS SOBRE UN TRATAMIENTO//
+const flowDuda = addKeyword(EVENTS.ACTION)
+    .addAnswer('Si tiene dudas sobre su tratamiento, llame *directamente* al +18097758194, gracias.');
+
+//OPCIOM 5 DEL MENU / CAMBIOS EN LA RECETA//
+const flowReceta = addKeyword(EVENTS.ACTION)
+    .addAnswer('Si requiere de cambios en la receta o necesita una licencia medica, por favor, llamar al 8096225457, *Lunes* a *Viernes* de 8:00AM a 5:00PM, gracias.');
+
+//OPCION 6 DEL MENU / EMBARAZADA//
+const flowEmb = addKeyword(EVENTS.ACTION)
+    .addAnswer('Si tiene alguna emergencia, llame *directamente* al +18097758194 o al +18297927117, gracias.');
+
+//OPCIOM 7 DEL MENU / OTRO//
+const flowOtro = addKeyword(EVENTS.ACTION)
+    .addAnswer('Gracias por comunicarse con el Dr. Baralt, en caso de necesitar mas informacion, por favor, llamar al 8096225457, *Lunes* a *Viernes* de 8:00AM a 5:00PM, gracias.');
+
+const menuFlow = addKeyword(['menu','Menu','Menú']).addAnswer(
     menu,
     { capture: true },
     async (ctx, { gotoFlow, fallBack, flowDynamic }) => {
-        if (!["1", "2", "3", "4", "5", "6", "7", "0"].includes(ctx.body)) {
-            return fallBack(
-                "Respuesta no válida, por favor selecciona una de las opciones."
-            );
-        }
-        switch (ctx.body) {
-            case "1":
-                return gotoFlow(flowCuenta);
-            case "2":
-                return gotoFlow(flowServ);
-            case "3":
-                return gotoFlow(flowPap);
-            case "4":
-                return gotoFlow(flowDuda);
-            case "5":
-                return gotoFlow(flowReceta);
-            case "6":
-                return gotoFlow(flowEmb);
-            case "7":
-                return gotoFlow(flowOtro);
-            case "0":
-                return await flowDynamic(
-                    "Saliendo... Puedes volver a acceder a este menú escribiendo *menu*"
-                );
-        }
+      if (!["1", "2", "3", "4", "5", "6", "7", "0"].includes(ctx.body)) {
+        return fallBack(
+          "Respuesta no válida, por favor selecciona una de las opciones."
+        );
+      }
+      switch (ctx.body) {
+        case "1":
+          return gotoFlow(flowCuenta);
+        case "2":
+          return gotoFlow(flowServ);
+        case "3":
+          return gotoFlow(flowPap);
+        case "4":
+          return gotoFlow(flowDuda);
+        case "5":
+          return gotoFlow(flowReceta);
+        case "6":
+          return gotoFlow(flowEmb);
+        case "7":
+            return gotoFlow(flowOtro);
+        case "0":
+          return await flowDynamic(
+            "Saliendo... Puedes volver a acceder a este menú escribiendo *menu*"
+          );
+      }
     }
 );
 
 const main = async () => {
-    const adapterDB = new MockAdapter();
-    const adapterFlow = createFlow([flowPrincipal, menuFlow, flowDuda, flowCuenta, flowOtro, flowReceta, cuentasFlow, flowCuentasB, flowCuentasBHD, flowCuentasP, flowServ]);
-    const adapterProvider = createProvider(BaileysProvider);
+    const adapterDB = new MockAdapter
+    const adapterFlow = createFlow([flowPrincipal, menuFlow, flowDuda, flowCuenta, flowOtro, flowReceta, cuentasFlow, flowCuentasB, flowCuentasBHD, flowCuentasP, flowServ])
+    const adapterProvider = createProvider(BaileysProvider)
 
     createBot({
         flow: adapterFlow,
         provider: adapterProvider,
         database: adapterDB,
-    });
+    })
 
-    QRPortalWeb();
+    QRPortalWeb()
 }
 
-main();
+main()
